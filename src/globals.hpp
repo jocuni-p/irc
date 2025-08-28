f86b98a21456a9947ffdff329e88983f949a916c
jocuni-p <jocuni-p@cbr1s4.42barcelona.com>
2025-08-28 11:30:22 +0200

    created 'include' directory

--- src/globals.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALS_HPP
-#define GLOBALS_HPP
-
-#include <csignal> // para sig_atomic_t
-
-// Para que se pueda ver esta variable desde Server.cpp
-extern volatile sig_atomic_t g_running;
-
-#endif
